7d8580bd6988bd1ccfb515d9681f7f537018704c
juaanjol19 <105843432+juaanjol19@users.noreply.github.com>
2023-02-09 01:28:39 -0600

    Add files via upload

---
+++ TiposDeDatosPrimitivos.java
@@ -0,0 +1,33 @@
+public class TiposDeDatosPrimitivos {
+    public static void main(String[]args){
+       // Variables de tipo entero
+       byte bytee = 123;
+       System.out.println("Este es un tipo de dato byte: "+ bytee);
+
+       short shortt = 12345;
+       System.out.println("Este es un tipo de dato short: "+ shortt);
+
+       int intt = 1234567890;
+       System.out.println("Este es un tipo de dato int: "+ intt);
+
+       long longg = 1234567891234567L;
+       System.out.println("Este es un tipo de dato long: "+longg);
+
+       // Variable de tipo decimal
+
+       float floatt = 3.33f;
+       System.out.println("Este es un tipo de dato floar: "+floatt);
+       
+       double doublee = 3.369369369;
+       System.out.println("Este es un tipo de dato double: "+ doublee);
+
+       // Variable de tipo caracter
+       char charr = 'G';
+       System.out.println("Este es un tipo de dato caracter: "+charr);
+
+       // variable booleana
+       boolean boleann = false;
+       System.out.println("Este es un tipo de dato boleano: "+ boleann);
+    }
+    
+}
