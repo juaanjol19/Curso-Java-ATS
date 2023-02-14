26d5ae244d1aaa7532fe83e15b4c7bb030cb893a
juaanjol19 <105843432+juaanjol19@users.noreply.github.com>
2023-02-13 23:42:42 -0600

    Add files via upload

---
+++ Constantes.java
@@ -0,0 +1,9 @@
+public class Constantes{
+    public static void main(String[] args){
+        final int numero = 10; // Conatante usamos la palabra fnal
+        System.out.println(numero);
+
+        
+
+    }
+}
